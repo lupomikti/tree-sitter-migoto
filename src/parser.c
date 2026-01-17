b34b4217dfa2d74d42339c8086499bd9a75c063d
lupomikti <25500382+lupomikti@users.noreply.github.com>
2026-01-16 22:57:54 -0800

    (release): version 0.5.0

--- src/parser.c
+++ src/parser.c
@@ -55219,8 +55219,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_migoto(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 4,
-      .patch_version = 4,
+      .minor_version = 5,
+      .patch_version = 0,
     },
   };
   return &language;
