c3a2682122e98738b0149d4039ccc12c1c4678ee
lupomikti <25500382+lupomikti@users.noreply.github.com>
2026-02-16 10:36:41 -0800

    release: version 0.5.7

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 LANGUAGE_NAME := tree-sitter-migoto
 HOMEPAGE_URL := https://github.com/lupomikti/tree-sitter-migoto
-VERSION := 0.5.6
+VERSION := 0.5.7
 
 # repository
 SRC_DIR := src
--- src/parser.c
+++ src/parser.c
@@ -60230,7 +60230,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_migoto(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 5,
-      .patch_version = 6,
+      .patch_version = 7,
     },
   };
   return &language;
