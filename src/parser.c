9d903374720affa8d9b932091fb6888d4989f79c
lupomikti <25500382+lupomikti@users.noreply.github.com>
2026-05-10 09:29:20 -0700

    release: version 0.7.2

--- src/parser.c
+++ src/parser.c
@@ -69437,7 +69437,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_migoto(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 7,
-      .patch_version = 1,
+      .patch_version = 2,
     },
   };
   return &language;
